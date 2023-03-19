0460af015bd4a52bf1305db7ea006deafe38f0dc
Vesa Laakso <482561+valscion@users.noreply.github.com>
2023-03-19 22:09:02 +0200

    Get records in party-people pages

--- app/party-people/[partyPerson]/page.tsx
+++ app/party-people/[partyPerson]/page.tsx
@@ -3,10 +3,50 @@ type Props = {
   params: Params;
 };
 
-export default function PartyPersonPage({ params }: Props) {
+type AirtableItem = {
+  /** The ID representing this record in Airtable */
+  id: string;
+  fields: {
+    Country: string;
+    Ordinal: number;
+    /** Duration is in seconds */
+    Duration: number;
+    Artist: string;
+    Song: string;
+    /** Flag is the emoji representation of the country */
+    Flag: string;
+    "Video type": string;
+    YouTube: string;
+  };
+};
+
+type AirtableData = Array<AirtableItem>;
+
+async function getDataFromAirtable(): Promise<AirtableData> {
+  const res = await fetch("http://localhost:3000/api/airtable-party-person", {
+    cache: "no-store",
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function PartyPersonPage({ params }: Props) {
+  const data = await getDataFromAirtable();
+
   return (
-    <div className="text-3xl font-bold underline">
-      PartyPerson: {params.partyPerson}
-    </div>
+    <>
+      <div className="text-3xl font-bold underline">
+        PartyPerson: {params.partyPerson}
+      </div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
   );
 }
