e4bc07712968217492e977d0bc5af04663180ea9
Vesa Laakso <482561+valscion@users.noreply.github.com>
2023-03-23 21:07:45 +0200

    Move all API alls inside the extracted module

--- app/apiCalls.ts
+++ app/apiCalls.ts
@@ -1,7 +1,23 @@
-import { AirtablePerson } from "./types/airtable";
+import { AirtablePerson, AirtableRecord } from "./types/airtable";
 
 export async function getPeopleFromAirtable(): Promise<AirtablePerson[]> {
   const res = await fetch("http://localhost:3000/api/airtable-party-people");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export async function getRecordsFromAirtable(): Promise<AirtableRecord[]> {
+  const res = await fetch("http://localhost:3000/api/airtable-party-person", {
+    cache: "no-store",
+  });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
--- app/party-people/[partyPerson]/page.tsx
+++ app/party-people/[partyPerson]/page.tsx
@@ -1,27 +1,10 @@
-import { type AirtableRecord } from "@/app/types/airtable";
 import { Ranking } from "./Ranking";
-import { getPeopleFromAirtable } from "@/app/apiCalls";
+import { getPeopleFromAirtable, getRecordsFromAirtable } from "@/app/apiCalls";
 
 type Params = { partyPerson: string };
 type Props = {
   params: Params;
 };
-
-async function getRecordsFromAirtable(): Promise<AirtableRecord[]> {
-  const res = await fetch("http://localhost:3000/api/airtable-party-person", {
-    cache: "no-store",
-  });
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json();
-}
 
 export default async function PartyPersonPage({ params }: Props) {
   const records = await getRecordsFromAirtable();
