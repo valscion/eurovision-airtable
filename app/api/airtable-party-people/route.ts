386910afdebc6e6f21f324cb9c1f93fcfed2885a
Vesa Laakso <482561+valscion@users.noreply.github.com>
2025-03-30 14:20:37 +0300

    Update base IDs to point to 2025

--- app/api/airtable-party-people/route.ts
+++ app/api/airtable-party-people/route.ts
@@ -79,7 +79,7 @@ export async function GET(request: Request) {
     apiKey: process.env.AIRTABLE_API_KEY,
   });
   // The Airtable base where all data lives in
-  const baseId = "appCLDjlqkm14rz6v";
+  const baseId = "appySnnTMCRrRbkDo";
   // The table ID which contains the wanted stuff
   const tableId = "tbl1LBikEbRZeXNEg";
 
